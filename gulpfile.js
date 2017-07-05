5bea5c838536ac8a8fe38eae7ac724ee4964e8c8
stauntonknight <nitkkr.vivek@gmail.com>
2017-07-05 00:16:40 -0700

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,16 @@ var ts = require('gulp-typescript');
 var watch = require('gulp-watch');
 var stylus = require('stylus');
 var inlineNg2Template = require('gulp-inline-ng2-template');
-var browserify = require('gulp-browserify');
+var browserify = require('browserify')
+var uglify = require('gulp-uglify');
+var rename = require("gulp-rename");
+var streamify = require('gulp-streamify')
+var clean = require('gulp-clean');
+var source = require('vinyl-source-stream')
 
 var tsProject = ts.createProject('tsconfig.json');
 
-gulp.task('build', function() {
+gulp.task('build', () => {
   var tsResult = tsProject.src().pipe(inlineNg2Template({
     'base': 'src/app',
     'styleProcessor': (path, extension, file, callback) => {
@@ -18,18 +23,24 @@ gulp.task('build', function() {
 });
 
 gulp.task('browserify', ['build'], () => {
-  gulp.src('build/src/main.js').pipe(browserify({
-    insertGlobals : true,
-    debug : !gulp.env.production
-  })).pipe(gulp.dest('dist'));
+  var stream = browserify('build/src/main.js').bundle();
+  stream
+      .pipe(source('main.js'))
+      .pipe(streamify(uglify()))
+      .on('error', (err) => { console.log(err);})
+      .pipe(gulp.dest('dist'));
 });
 
+gulp.task('clean', () => {
+  gulp.src('build', {read:false}).pipe(clean());
+  gulp.src('dist', {read:false}).pipe(clean());
+});
 
 gulp.task('compile', ['browserify'], () => {
   console.log('done');
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', () => {
   return watch([
     "*.ts", "**/*.ts", "*.html", "**/*.html", "**/*.css", "**/*.styl",
     "!./node_modules"],
